44603ab8773f3504bc120538e66c7297985aa77b
felinatriste <160907958+felinatriste@users.noreply.github.com>
2024-02-26 17:04:46 -0300

    laron

---
+++ amolaron.py
@@ -0,0 +1,4 @@
+print("Isto é amor, grande amor, O amor com que tanto eu sonhei.")
+print("Ouça com o coração, e você entenderá.")
+resposta = input("eu amo: ")
+print("laron ama drika e eu", resposta)
